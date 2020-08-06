6e8c9ecf9b1bb5cae33694d31186641388a0f95e
panetony <paneotny@outlook.com.br>
2020-08-06 15:51:20 -0300

    version b

--- IProblemDetailValidation.cs
+++ IProblemDetailValidation.cs
@@ -1,6 +1,12 @@
-﻿namespace Panetony.ProblemDetails
+﻿using System.Collections.Generic;
+
+namespace Panetony.ProblemDetails
 {
     public interface IProblemDetailValidation
     {
+        void Add(string code, string title = "", string detail = "");
+        int Count();
+        bool HasMessage();
+        IList<ProblemDetail> Value();
     }
 }
--- ProblemDetailValidation.cs
+++ ProblemDetailValidation.cs
@@ -5,5 +5,33 @@ namespace Panetony.ProblemDetails
     public class ProblemDetailValidation : IProblemDetailValidation
     {
         private IList<ProblemDetail> _listOfProblemDetail = new List<ProblemDetail>();
+
+        public ProblemDetailValidation()
+        {
+        }
+        public ProblemDetailValidation(ProblemDetail ProblemDetail)
+        {
+            _listOfProblemDetail.Add(ProblemDetail);
+        }
+        public ProblemDetailValidation(string code, string title = "", string detail = "")
+        {
+            _listOfProblemDetail.Add(new ProblemDetail(code, title, detail));
+        }
+        public void Add(string code, string title = "", string detail = "")
+        {
+            _listOfProblemDetail.Add(new ProblemDetail(code, title, detail));
+        }
+        public int Count()
+        {
+            return _listOfProblemDetail.Count;
+        }
+        public bool HasMessage()
+        {
+            return Count() > 0;
+        }
+        public IList<ProblemDetail> Value()
+        {
+            return _listOfProblemDetail;
+        }
     }
 }
