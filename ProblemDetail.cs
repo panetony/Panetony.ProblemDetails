7bd38fce466cd43a82cd0922fa10433fb6902c04
panetony <paneotny@outlook.com.br>
2020-08-08 18:42:30 -0300

    refactoring...

--- ProblemDetail.cs
+++ ProblemDetail.cs
@@ -2,16 +2,26 @@
 {
     public class ProblemDetail
     {
-        public string Code { get; private set; }
+        public string Code { get; set; }
         public string Title { get; set; }
         public string Detail { get; set; }
+
+        public ProblemDetail()
+        {
+        }
 
         public ProblemDetail(string code)
         {
             Code = code;
         }
 
-        public ProblemDetail(string code, string title = "", string detail = "")
+        public ProblemDetail(string code, string title)
+        {
+            Code = code;
+            Title = title;
+        }
+
+        public ProblemDetail(string code, string title, string detail)
         {
             Code = code;
             Title = title;
